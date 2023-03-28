2d56291dc0309c6f3c325c39ba80b33d0e5fb381
Timothy Axford <t.axford2@yahoo.com>
2023-03-27 21:26:11 -0400

    Added using System.Collections to get started

--- ArrayList.Net5.0/Program.cs
+++ ArrayList.Net5.0/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace ArrayList.Net5._0
 {
@@ -6,6 +7,8 @@ namespace ArrayList.Net5._0
     {
         static void Main(string[] args)
         {
+            // declaring an ArrayList
+
         }
     }
 }
